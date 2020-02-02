6fe57b68bbde9290a4a1823649206543d3e190b3
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-02-02 12:34:11 -0500

    Fix multiple argument names
    Fix a syntax issue where the short or long forms of arguments was not
    properly being read. Add return statement for arguments.

--- pymaengda/arguments.py
+++ pymaengda/arguments.py
@@ -9,13 +9,16 @@ class Arguments():
     def parse_args(self):
         print('parse_args')
         self.parser.add_argument(
-                '-a, --amount',
+                '-a', '--amount',
                 type=float,
                 help='amount (in grams) of the burn',
                 dest='amount')
         self.parser.add_argument(
-                '-s, --strain',
+                '-s', '--strain',
+                type=str,
                 help='strain name',
                 dest='strain')
         self.args = self.parser.parse_args()
         print(f"self.args.amount='{self.args.amount}', self.args.strain='{self.args.strain}'")
+
+        return self.args
