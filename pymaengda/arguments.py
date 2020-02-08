4b85efabb4d4c1fc2c73c32f40cdea26e27999dd
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-02-08 14:13:02 -0500

    Begin implementing argument sub parsers
    Using the sub-parsers provided by argparse we are able to achieve nicer
    argument formatting. The basics of it is that users will select a mode
    for the program to operate in followed by any required arguments. This
    is done without using flags for the required arguments, only for the
    optional ones.
    
    This enables 'nicer' commands such as:
      sh bin/pymaengda burn 3.5 super-green
    or
      sh bin/pymaengda log --limit 25

--- pymaengda/arguments.py
+++ pymaengda/arguments.py
@@ -10,6 +10,7 @@ class Arguments():
                 https://github.com/ncdulo/pymaengda
                 '''
             )
+        self.subparsers = self.parser.add_subparsers(help='modes')
 
     def parse_args(self):
         print('parse_args')
@@ -20,16 +21,27 @@ class Arguments():
                 type=int,
                 help='runlevel to start in',
                 dest='runlevel')
-        self.parser.add_argument(
-                '-a', '--amount',
+        #self.parser.add_argument(
+        #        '-a', '--amount',
+        #        type=float,
+        #        help='amount (in grams) of the burn',
+        #        dest='amount')
+        #self.parser.add_argument(
+        #        '-s', '--strain',
+        #        type=str,
+        #        help='strain name',
+        #        dest='strain')
+        self.burn_parser = self.subparsers.add_parser(
+                'burn', help='Add a burn')
+        self.burn_parser.add_argument(
+                'amount',
                 type=float,
-                help='amount (in grams) of the burn',
-                dest='amount')
-        self.parser.add_argument(
-                '-s', '--strain',
+                help='Amount in grams')
+        self.burn_parser.add_argument(
+                'strain',
                 type=str,
-                help='strain name',
-                dest='strain')
+                help='Name of strain burned')
+
         self.args = self.parser.parse_args()
         print(f"self.args.runlevel='{self.args.runlevel}'")
         print(f"self.args.amount='{self.args.amount}', self.args.strain='{self.args.strain}'")
